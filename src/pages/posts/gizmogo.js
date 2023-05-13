abdad4820c5a3f44d468c81c0ca250415e65e96d
ben40d9 <benpaley6@gmail.com>
2023-05-12 23:04:08 -0400

    Implemented Gizmogo landing page Developed Gizmogo.js component for email capture form Applied custom styling to the Gizmogo landing page using CSS modules

--- src/pages/posts/gizmogo.js
+++ src/pages/posts/gizmogo.js
@@ -1,7 +1,78 @@
-// https://gizmogo.pxf.io/ethosPromos
+// // https://gizmogo.pxf.io/ethosPromos
 
+// import { useState } from "react";
+// import styles from "./NameCheap.module.css";
+
+// const Gizmogo = () => {
+//   const [email, setEmail] = useState("");
+
+//   const validateForm = async (event) => {
+//     event.preventDefault();
+//     const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+//     if (!regex.test(email)) {
+//       alert("Please enter a valid email address.");
+//       return;
+//     }
+//     sendData(email);
+//   };
+
+//   const sendData = async (email) => {
+//     const response = await fetch(`/api/submit_email/gizmongo`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ email }),
+//     });
+//     if (response.ok) {
+//       //for now it has an alert pop-up but I want to change this so that the user does not have to click okay to get to the affiliate link
+//       alert("Thank you! Your email has been submitted.");
+//       // Redirect to the affiliate link after submission
+//       window.location.href = "https://gizmogo.pxf.io/ethosPromos";
+//     } else {
+//       alert("Something went wrong. Please try again.");
+//     }
+//   };
+
+//   return (
+//     <div className={styles.container}>
+//       <a
+//         href="https://namecheap.pxf.io/c/4197431/408988/5618"
+//         target="_top"
+//         id="408988"
+//         className={styles.logoLink}
+//       >
+//         <img
+//           className={styles.logo}
+//           src="//a.impactradius-go.com/display-ad/5618-408988"
+//           alt="Namecheap"
+//         />
+//       </a>
+//       <h1 className={styles.heading}>Get NameCheap Now!</h1>
+//       <p className={styles.subheading}>
+//         Don't miss out on our limited-time offer.
+//       </p>
+//       <form onSubmit={validateForm} className={styles.form}>
+//         <input
+//           type="email"
+//           name="email"
+//           placeholder="Enter your email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//           required
+//           className={styles.emailInput}
+//         />
+//         <button type="submit" className={styles.submitButton}>
+//           Claim Offer
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Gizmogo;
 import { useState } from "react";
-import styles from "./NameCheap.module.css";
+import styles from "./Gizmogo.module.css";
 
 const Gizmogo = () => {
   const [email, setEmail] = useState("");
@@ -17,7 +88,7 @@ const Gizmogo = () => {
   };
 
   const sendData = async (email) => {
-    const response = await fetch(`/api/submit_email/gizmongo`, {
+    const response = await fetch(`/api/submit_email/gizmogo`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -25,9 +96,7 @@ const Gizmogo = () => {
       body: JSON.stringify({ email }),
     });
     if (response.ok) {
-      //for now it has an alert pop-up but I want to change this so that the user does not have to click okay to get to the affiliate link
       alert("Thank you! Your email has been submitted.");
-      // Redirect to the affiliate link after submission
       window.location.href = "https://gizmogo.pxf.io/ethosPromos";
     } else {
       alert("Something went wrong. Please try again.");
@@ -37,18 +106,27 @@ const Gizmogo = () => {
   return (
     <div className={styles.container}>
       <a
-        href="https://namecheap.pxf.io/c/4197431/408988/5618"
+        href="https://gizmogo.pxf.io/c/4197431/1604433/17499"
         target="_top"
-        id="408988"
+        id="1604433"
         className={styles.logoLink}
       >
         <img
           className={styles.logo}
-          src="//a.impactradius-go.com/display-ad/5618-408988"
-          alt="Namecheap"
+          src="//a.impactradius-go.com/display-ad/17499-1604433"
+          alt=""
+          width="468"
+          height="60"
         />
       </a>
-      <h1 className={styles.heading}>Get NameCheap Now!</h1>
+      <img
+        height="0"
+        width="0"
+        src="https://imp.pxf.io/i/4197431/1604433/17499"
+        style={{ position: "absolute", visibility: "hidden" }}
+        border="0"
+      />
+      <h1 className={styles.heading}>Get Gizmogo Now!</h1>
       <p className={styles.subheading}>
         Don't miss out on our limited-time offer.
       </p>
