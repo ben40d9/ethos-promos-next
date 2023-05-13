3801e349a3a455612926e75239602457799f83e1
ben40d9 <benpaley6@gmail.com>
2023-05-12 23:03:53 -0400

    Added NordVPN landing page Created NordVPN.js component for email capture form Styled the NordVPN landing page using CSS modules

--- src/pages/posts/nord.js
+++ src/pages/posts/nord.js
@@ -1,23 +1,5 @@
-// export default function NordVpn() {
-//   return <h1>First Post</h1>;
-// }
-
-// import Link from "next/link";
-
-// export default function NordVpn() {
-//   return (
-//     <>
-//       <h1>NordVPN</h1>
-//       <div></div>
-//       <h2>
-//         <Link href="/">Back to home</Link>
-//       </h2>
-//     </>
-//   );
-// }
-
 import { useState } from "react";
-import styles from "./NameCheap.module.css";
+import styles from "./Nord.module.css";
 
 const NordVPN = () => {
   const [email, setEmail] = useState("");
@@ -29,10 +11,6 @@ const NordVPN = () => {
       alert("Please enter a valid email address.");
       return;
     }
-    sendData(email);
-  };
-
-  const sendData = async (email) => {
     const response = await fetch(`/api/submit_email/nordvpn`, {
       method: "POST",
       headers: {
@@ -41,9 +19,7 @@ const NordVPN = () => {
       body: JSON.stringify({ email }),
     });
     if (response.ok) {
-      //for now it has an alert pop-up but I want to change this so that the user does not have to click okay to get to the affiliate link
       alert("Thank you! Your email has been submitted.");
-      // Redirect to the affiliate link after submission
       window.location.href = "https://nordvpn.sjv.io/ethosPromos";
     } else {
       alert("Something went wrong. Please try again.");
@@ -53,18 +29,26 @@ const NordVPN = () => {
   return (
     <div className={styles.container}>
       <a
-        href="https://namecheap.pxf.io/c/4197431/408988/5618"
+        href="https://nordvpn.sjv.io/c/4197431/493823/7452"
         target="_top"
-        id="408988"
-        className={styles.logoLink}
+        id="493823"
       >
         <img
-          className={styles.logo}
-          src="//a.impactradius-go.com/display-ad/5618-408988"
-          alt="Namecheap"
+          src="//a.impactradius-go.com/display-ad/7452-493823"
+          border="0"
+          alt=""
+          width="144"
+          height="144"
         />
       </a>
-      <h1 className={styles.heading}>Get NameCheap Now!</h1>
+      <img
+        height="0"
+        width="0"
+        src="https://nordvpn.sjv.io/i/4197431/493823/7452"
+        style={{ position: "absolute", visibility: "hidden" }}
+        border="0"
+      />
+      <h1 className={styles.heading}>Get NordVPN Now!</h1>
       <p className={styles.subheading}>
         Don't miss out on our limited-time offer.
       </p>
@@ -87,3 +71,64 @@ const NordVPN = () => {
 };
 
 export default NordVPN;
+
+// import { useState } from "react";
+// import styles from "./Nord.module.css";
+
+// const NordVPN = () => {
+//   const [email, setEmail] = useState("");
+
+//   const validateForm = async (event) => {
+//     event.preventDefault();
+//     const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+//     if (!regex.test(email)) {
+//       alert("Please enter a valid email address.");
+//       return;
+//     }
+//     const response = await fetch(`/api/submit_email/nordvpn`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ email }),
+//     });
+//     if (response.ok) {
+//       alert("Thank you! Your email has been submitted.");
+//       window.location.href = "https://nordvpn.sjv.io/ethosPromos";
+//     } else {
+//       alert("Something went wrong. Please try again.");
+//     }
+//   };
+
+//   return (
+//     <div className={styles.container}>
+//       <a href="https://nordvpn.com/" target="_top" className={styles.logoLink}>
+//         <img
+//           className={styles.logo}
+//           src="/images/nordvpn-logo.svg"
+//           alt="NordVPN"
+//         />
+//       </a>
+//       <h1 className={styles.heading}>Get NordVPN Now!</h1>
+//       <p className={styles.subheading}>
+//         Don't miss out on our limited-time offer.
+//       </p>
+//       <form onSubmit={validateForm} className={styles.form}>
+//         <input
+//           type="email"
+//           name="email"
+//           placeholder="Enter your email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//           required
+//           className={styles.emailInput}
+//         />
+//         <button type="submit" className={styles.submitButton}>
+//           Claim Offer
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default NordVPN;
