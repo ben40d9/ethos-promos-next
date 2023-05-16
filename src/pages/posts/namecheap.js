64a19aaf62ed4ee592fc9c71a28028bf9bdfda78
ben40d9 <benpaley6@gmail.com>
2023-05-16 00:09:58 -0700

    stupid backround img wont be the backround. want form on top on backround. will get to in the AM

--- src/pages/posts/namecheap.js
+++ src/pages/posts/namecheap.js
@@ -23,8 +23,6 @@ const NameCheap = () => {
       body: JSON.stringify({ email }),
     });
     if (response.ok) {
-      //for now it has an alert pop-up but I want to change this so that the user does not have to click okay to get to the affiliate link
-      alert("Thank you! Your email has been submitted.");
       // Redirect to the affiliate link after submission
       window.location.href = "https://namecheap.pxf.io/5g53bD";
     } else {
@@ -46,101 +44,28 @@ const NameCheap = () => {
           alt="Namecheap"
         />
       </a>
-      <h1 className={styles.heading}>Get NameCheap Now!</h1>
-      <p className={styles.subheading}>
-        Don't miss out on our limited-time offer.
-      </p>
-      <form onSubmit={validateForm} className={styles.form}>
-        <input
-          type="email"
-          name="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          className={styles.emailInput}
-        />
-        <button type="submit" className={styles.submitButton}>
-          Claim Offer
-        </button>
-      </form>
+      <div className={styles.formContainer}>
+        <h1 className={styles.heading}>Get NameCheap Now!</h1>
+        <p className={styles.subheading}>
+          Don't miss out on our limited-time offer.
+        </p>
+        <form onSubmit={validateForm} className={styles.form}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className={styles.emailInput}
+          />
+          <button type="submit" className={styles.submitButton}>
+            Claim Offer
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
 
 export default NameCheap;
-
-// import { useState } from "react";
-
-// const NameCheap = () => {
-//   const [email, setEmail] = useState("");
-
-//   const validateForm = async (event) => {
-//     event.preventDefault();
-//     const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-//     if (!regex.test(email)) {
-//       alert("Please enter a valid email address.");
-//       return;
-//     }
-//     sendData(email);
-//   };
-
-//   const sendData = async (email) => {
-//     const response = await fetch("/api/submit_email", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ email }),
-//     });
-
-//     if (response.ok) {
-//       alert("Thank you! Your email has been submitted.");
-//       // Redirect to the affiliate link after submission
-//       window.location.href = "https://namecheap.pxf.io/5g53bD";
-//     } else {
-//       alert("Something went wrong. Please try again.");
-//     }
-//   };
-
-//   return (
-//     <div className="container" style={{ backgroundColor: "#f0f4f6" }}>
-//       <a
-//         href="https://namecheap.pxf.io/c/4197431/408988/5618"
-//         target="_top"
-//         id="408988"
-//       >
-//         <img
-//           className="logo"
-//           src="//a.impactradius-go.com/display-ad/5618-408988"
-//           border="0"
-//           alt="Namecheap"
-//           width="182"
-//           height="103"
-//         />
-//       </a>
-//       <img
-//         height="0"
-//         width="0"
-//         src="https://namecheap.pxf.io/i/4197431/408988/5618"
-//         style={{ position: "absolute", visibility: "hidden" }}
-//         border="0"
-//       />
-//       <h1>Get NameCheap Now!</h1>
-//       <form onSubmit={validateForm}>
-//         <input
-//           type="email"
-//           name="email"
-//           placeholder="Enter your email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//           required
-//           style={{ color: "black" }} // Fix for white text issue
-//         />
-//         <input type="submit" value="Submit" />
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default NameCheap;
