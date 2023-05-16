7097589b4b6355c85175f52b30388fcb5673f9ff
ben40d9 <benpaley6@gmail.com>
2023-05-15 23:42:32 -0700

    golfPartner updated, state & style

--- src/pages/posts/golfpartner.js
+++ src/pages/posts/golfpartner.js
@@ -23,8 +23,10 @@
 //       body: JSON.stringify({ email }),
 //     });
 //     if (response.ok) {
+//       //for now it has an alert pop-up but I want to change this so that the user does not have to click okay to get to the affiliate link
 //       alert("Thank you! Your email has been submitted.");
-//       window.location.href = "https://golfpartner.com/";
+//       // Redirect to the affiliate link after submission
+//       window.location.href = "https://golfpartnerusa.com/";
 //     } else {
 //       alert("Something went wrong. Please try again.");
 //     }
@@ -32,9 +34,21 @@
 
 //   return (
 //     <div className={styles.container}>
-//       <h1 className={styles.heading}>Join GolfPartner Today!</h1>
+//       <a
+//         href="https://golfpartnerusa.com/"
+//         target="_top"
+//         id="golfpartner"
+//         className={styles.logoLink}
+//       >
+//         <img
+//           className={styles.logo}
+//           src="../../images/golfPartnerLogo.png"
+//           alt="GolfPartner"
+//         />
+//       </a>
+//       <h1 className={styles.heading}>Get GolfPartner Now!</h1>
 //       <p className={styles.subheading}>
-//         Sign up to receive exclusive golf deals and promotions.
+//         Don't miss out on our limited-time offer.
 //       </p>
 //       <form onSubmit={validateForm} className={styles.form}>
 //         <input
@@ -47,7 +61,7 @@
 //           className={styles.emailInput}
 //         />
 //         <button type="submit" className={styles.submitButton}>
-//           Join Now
+//           Claim Offer
 //         </button>
 //       </form>
 //     </div>
@@ -55,11 +69,13 @@
 // };
 
 // export default GolfPartner;
+
 import { useState } from "react";
 import styles from "./GolfPartner.module.css";
 
 const GolfPartner = () => {
   const [email, setEmail] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
   const validateForm = async (event) => {
     event.preventDefault();
@@ -80,17 +96,23 @@ const GolfPartner = () => {
       body: JSON.stringify({ email }),
     });
     if (response.ok) {
-      //for now it has an alert pop-up but I want to change this so that the user does not have to click okay to get to the affiliate link
-      alert("Thank you! Your email has been submitted.");
-      // Redirect to the affiliate link after submission
-      window.location.href = "https://golfpartnerusa.com/";
+      setSubmitted(true);
     } else {
       alert("Something went wrong. Please try again.");
     }
   };
 
+  if (submitted) {
+    window.location.href = "https://golfpartnerusa.com/";
+  }
+
   return (
     <div className={styles.container}>
+      <img
+        className={styles.backgroundImage}
+        src="../../images/welcomeToGizmogo.png"
+        alt="Background Image"
+      />
       <a
         href="https://golfpartnerusa.com/"
         target="_top"
