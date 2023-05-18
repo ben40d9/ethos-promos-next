7b020d07a065a109d45fcec8329bb15cdd5f167d
ben40d9 <benpaley6@gmail.com>
2023-05-18 02:15:07 -0700

    big progress error is now "  {     email: 'benpaley6@gmail.com',     status: 'rejected',     _id: '2e7e1a8235d44715bb3a6adec3cc334d',     reject_reason: 'unsigned',     queued_reason: null   }"

--- src/utils/mailchimp.js
+++ src/utils/mailchimp.js
@@ -1,60 +1,26 @@
-const mailchimp = require("@mailchimp/mailchimp_marketing");
-const generateContent = require("./generateContent");
+// const mailchimp = require("@mailchimp/mailchimp_marketing");
+// const generateContent = require("./generateContent");
 
-require("dotenv").config();
+// require("dotenv").config();
 
-mailchimp.setConfig({
-  apiKey: "1c05fd309a1fe864251991d6d8376470-us21",
-  server: "us21", // The server prefix is the part after the dash in your API key
-});
+// mailchimp.setConfig({
+//   apiKey: "1c05fd309a1fe864251991d6d8376470-us21",
+//   server: "us21", // The server prefix is the part after the dash in your API key
+// });
 
-async function callPing() {
-  const response = await mailchimp.ping.get();
-  console.log(response);
-}
+// async function callPing() {
+//   const response = await mailchimp.ping.get();
+//   console.log(response);
+// }
 
-callPing();
-
-async function sendEmail(email, company) {
-  const subject = `Welcome to ${company}!`;
-  const content = await generateContent(company);
-
-  try {
-    const response = await mailchimp.messages.send({
-      message: {
-        from_email: "ben@ethospromos.com",
-        subject: subject,
-        text: content,
-        to: [
-          {
-            email: email,
-            type: "to",
-          },
-        ],
-      },
-    });
-
-    console.log(response);
-  } catch (error) {
-    console.error("Failed to send email:", error);
-  }
-}
-
-module.exports = sendEmail;
-
-// const mailchimpTransactional = require("@mailchimp/mailchimp_transactional")(
-//   "1c05fd309a1fe864251991d6d8376470-us21"
-// );
-// const generateContent = require("./generateContent.js");
+// callPing();
 
 // async function sendEmail(email, company) {
 //   const subject = `Welcome to ${company}!`;
 //   const content = await generateContent(company);
 
 //   try {
-//     const response = await mailchimpTransactional.messages.send({
-//       template_content: [{}],
-//       template_name: "template_name",
+//     const response = await mailchimp.messages.send({
 //       message: {
 //         from_email: "ben@ethospromos.com",
 //         subject: subject,
@@ -75,3 +41,37 @@ module.exports = sendEmail;
 // }
 
 // module.exports = sendEmail;
+
+const mailchimpTransactional = require("@mailchimp/mailchimp_transactional")(
+  "md-zq0s64qrJFvJlW4JqPn2jg"
+);
+const generateContent = require("./generateContent.js");
+
+async function sendEmail(email, company) {
+  const subject = `Welcome to ${company}!`;
+  const content = await generateContent(company);
+
+  try {
+    const response = await mailchimpTransactional.messages.send({
+      template_content: [{}],
+      template_name: "template_name",
+      message: {
+        from_email: "ben@ethospromos.com",
+        subject: subject,
+        text: content,
+        to: [
+          {
+            email: email,
+            type: "to",
+          },
+        ],
+      },
+    });
+
+    console.log(response);
+  } catch (error) {
+    console.error("Failed to send email:", error);
+  }
+}
+
+module.exports = sendEmail;
