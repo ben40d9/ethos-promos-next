913af0332685ea55a49eedde5e77bf11c585e2ab
ben40d9 <benpaley6@gmail.com>
2023-05-22 23:11:45 -0700

    email is now being sent on submit but the variables, of the sendEmail function, are not working. so the email is being set with the contents set in the function defined in sendEmail.js file

--- src/pages/api/submit_email/[company].js
+++ src/pages/api/submit_email/[company].js
@@ -1,5 +1,9 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-import sendEmail from "../../../utils/mailchimp";
+
+import sendEmail from "../../../utils/sendEmail";
+
+// import generateContent from "../../../utils/generateContent";
+
 import "dotenv/config";
 
 //will hide the dbName but does not matter atm
@@ -20,13 +24,27 @@ export default async (req, res) => {
       const { company } = req.query; // Get the company name from the route parameters
 
       await client.connect();
+
       const db = client.db(dbName);
+
       const collection = db.collection(company); // Use the company name to determine the collection
 
       await collection.insertOne({ email });
 
+      // const content = await generateContent(company);
+      const content =
+        await `test here I am sending this email, to ${email}, inquiring about ${company} from the server`;
+
+      var msg = {
+        to: `${email}`, // Change to your recipient
+        from: "ben@ethospromos.com", // Change to your verified sender
+        subject: "I am Sending with SendGrid",
+        text: `${content}`,
+        html: "<strong>this is the new test</strong>",
+      };
+
       //send email
-      await sendEmail(email, company);
+      await sendEmail(msg, content);
 
       res.status(200).send("Email submitted successfully");
     } catch (err) {
