3eb4592c2cf11c84ec0529c6fe6925d63673fb90
ben40d9 <benpaley6@gmail.com>
2023-05-22 23:12:12 -0700

    for now just have blank. will deal with the content generation later

--- src/utils/generateContent.js
+++ src/utils/generateContent.js
@@ -1,18 +1,43 @@
 const { Configuration, OpenAIApi } = require("openai");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const configuration = new Configuration({
-  apiKey: `${process.env.OPENAI_API_KEY}`,
-});
+// console.log(process.env);
+// console.log(process.env.OPENAI_API_KEY);
 
-async function generateContent(company) {
-  const openai = await new OpenAIApi(configuration);
-  // const prompt = `Generate a promotional email content for ${company}.`;
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `Generate a promotional email content for ${company}.`,
-  });
+// const configuration = new Configuration({
+//   apiKey: `${process.env.OPENAI_API_KEY}`,
+// });
 
-  //   return response.choices[0].text.trim();
-}
+// async function generateContent(company) {
+//   const openai = await new OpenAIApi(configuration);
+//   // const prompt = `Generate a promotional email content for ${company}.`;
+//   const response = await openai.createCompletion({
+//     model: "text-davinci-003",
+//     prompt: `Generate a promotional email content for ${company}.`,
+//   });
 
-module.exports = generateContent;
+//   return response.choices[0].text.trim();
+// }
+
+// async function generateContent(company) {
+//   const openai = await new OpenAIApi(configuration);
+
+//   const response = await openai.createCompletion({
+//     model: "text-davinci-003",
+//     prompt: `Generate a promotional email content for ${company}.`,
+//   });
+
+//   console.log(response.data.choices[0].text.trim());
+
+//   if (response && response.choices && response.choices.length > 0) {
+//     return response.choices[0].text.trim();
+//   } else {
+//     console.error("Unexpected response from OpenAI API:", response);
+//     return "";
+//   }
+// }
+
+// generateContent("nordVPN");
+
+// module.exports = generateContent;
